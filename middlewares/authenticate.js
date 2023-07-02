c23bafbb07ab72eada2772cfde07175f27031d11
juliakarnauh <juliakarnayh04@gmail.com>
2023-07-02 15:29:31 +0300

    fix authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,24 +1,26 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const { SECRET_KEY } = process.env;
-const HttpError = require("../helpers/HttpError");
+
 const authenticate = async (req, res, next)=> {
     const {authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ");
     if(bearer !== "Bearer") {
-        next(HttpError(401))
+      res.status(401).json({ message: "Not authorized" });
+      next()
     }
     try {
         const {id} = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if(!user || !user.token || user.token !== token) {
-            next(HttpError(401))
+          res.status(401).json({ message: "Not authorized" });
+          next()
         }
         req.user = user;
         next();
     }
     catch {
-        next(HttpError(401));
+      res.status(401).json({ message: "Not authorized" });
     }
 }
 
